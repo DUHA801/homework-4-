3e8c166478ec660f91ab6cfff85f91a5335b7fc8
DUHA <duha160780@gmail.com>
2024-11-09 07:51:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+first_neme= 12
+clok=1.5
+second_neme= 'PYTON'
+time=(first_neme*clok)
+age='КУРС:'
+print(age, second_neme,"всего задач:",first_neme,'затрачено часов:',clok,'среднее время выполнения',time)
